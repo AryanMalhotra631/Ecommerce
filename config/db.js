e9168b2b4d21faf664b706e0ebdee174c72aba6c
Aryan Malhotra <aryanmalhotra631@gmail.com>
2023-08-04 12:36:59 -0700

    intial commit

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+import colors from 'colors'
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Connected to Mongo Db DataBase ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`error in MongoDb ${error}`.bgRed.white);
+    }
+}; export default connectDB;
